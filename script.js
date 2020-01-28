1b54f23228db2f4770770146a28928d69ef2834f
lynseyod <lyns.odonnell@gmail.com>
2020-01-28 12:30:40 -0500

    We got slide ins! We got polaroids! :art::bomb:

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let elementsToAnimate;
   let windowHeight;
+  let elementsToFallIn;
+  let elementsToSlideIn;
 
   const init = () => {
     elementsToFallIn = document.querySelectorAll('.to-fall-in');
+    elementsToSlideIn = document.querySelectorAll('.to-slide-in')
     windowHeight = window.innerHeight;
-    console.log(elementsToFallIn);
   }
 
   const checkPosition = () => {
     init();
-    console.log(elementsToFallIn)
     for (let i = 0; i < elementsToFallIn.length; i++) {
       const element = elementsToFallIn[i];
       const positionFromTop = element.getBoundingClientRect().top;
       if (positionFromTop - windowHeight <= 0) {
         element.classList.add(`fall-in-${i+1}`);
         element.classList.remove('to-fall-in')
+      }
+    }
+    for (let i = 0; i < elementsToSlideIn.length; i++) {
+      const element = elementsToSlideIn[i];
+      const positionFromTop = element.getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add('slide-in');
+        element.classList.remove('to-slide-in');
       }
     }
   }
